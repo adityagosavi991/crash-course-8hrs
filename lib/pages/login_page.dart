ad15a4b1a9243c5f708f3cc082d023ac37eea0bc
Aditya Gosavi <adityagosavi991@gmail.com>
2021-07-12 19:38:49 +0530

    Day-6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,60 +10,84 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  final _formKey = GlobalKey<
+      FormState>(); //A key created to access the form widget from anyplace in the code.
+
+  moveToHome(BuildContext context) {
+    // if(_formKey.currentState.validate()){
+
+    Navigator.pushNamed(context, MyRoutes.homeRoute);
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Welcome $name',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
-              child: Column(
-                children: [
-                  TextFormField(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Welcome $name',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter UserName", labelText: "UserName"),
                       onChanged: (value) {
                         name = value;
                         setState(() {});
-                      }),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: Text("Login"),
-                      style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue[900],
-                          padding: EdgeInsets.all(20),
-                          minimumSize: Size(150, 50))),
-                ],
-              ),
-            )
-          ],
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        onPressed: () => moveToHome(context),
+                        child: Text("Login"),
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.blue[900],
+                            padding: EdgeInsets.all(20),
+                            minimumSize: Size(150, 50))),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
