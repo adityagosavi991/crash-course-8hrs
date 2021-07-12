b2030e2824ef2c3678869af6296174e6c08e863e
Aditya Gosavi <adityagosavi991@gmail.com>
2021-07-12 10:59:59 +0530

    Day-4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_project/pages/home_page.dart';
 import 'package:ecommerce_project/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -13,6 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage(),
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       initialRoute:
           "/login", //This tells flutter to open login page first and not home page, which is by default due to "/" route.
       routes: {
